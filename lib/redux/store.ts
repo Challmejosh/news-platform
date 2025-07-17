19bfbd22fe59046581de640eb1cef8fb27da8497
Joshua Adediran <challmejosh@gmail.com>
2025-07-17 15:31:06 +0100

    Update store.ts

--- lib/redux/store.ts
+++ lib/redux/store.ts
@@ -1,15 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
-import { storiesApi } from "./api/storiesApi";
 import Bookmark from "./slice/bookmarkSlice"
 import toggle from "./slice/toggleSlice"
 export const store = configureStore({
   reducer: {
-    [storiesApi.reducerPath]: storiesApi.reducer,
     bookmark:Bookmark,
     toggle: toggle
   },
-  middleware: (getDefaultMiddleware) =>
-    getDefaultMiddleware().concat(storiesApi.middleware),
 });
 export type AppDispatch = typeof store.dispatch;
 export type RootState = ReturnType<typeof store.getState>;
