75264e2cd8cb38b9af7e8ae32c9d3e24139b47e6
Joshua Adediran <challmejosh@gmail.com>
2025-07-17 15:22:04 +0100

    Update AppLayout.tsx

--- Layout/AppLayout.tsx
+++ Layout/AppLayout.tsx
@@ -1,10 +1,11 @@
 "use client"
+import { client } from "@/lib/TansackQuery
 import { store } from "@/lib/redux/store";
 import { Provider } from "react-redux";
 import { QueryClientProvider } from "@tanstack/react-query
 const AppLayout = ({children}:{children:React.ReactNode}) => {
     return ( 
-        <QueryClientProvider client={} >
+        <QueryClientProvider client={client} >
         <Provider store={store}>
             {children}
         </Provider>
